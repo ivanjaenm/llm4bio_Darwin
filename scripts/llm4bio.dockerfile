66e8628da1f1a886680a3b87783c8209ac4682ac
Ivan JM <ivanjaenm@gmail.com>
2025-02-27 23:30:57 -0600

    updated scripts

--- scripts/llm4bio.dockerfile
+++ scripts/llm4bio.dockerfile
@@ -6,7 +6,8 @@ RUN set -xe && \
     apt-get -yqq install python3-pip && \
     pip3 install --upgrade pip && \
     apt-get -yqq install python3-venv && \
-    apt-get -yqq install rsync 
+    apt-get -yqq install rsync \
+    apt-get -y install git
 
 COPY requirements.txt.darwin /tmp/requirements.txt.darwin
 #ENV PATH="/home/software/bin:${PATH}"
--- scripts/run_experiment.sh
+++ scripts/run_experiment.sh
@@ -45,17 +45,6 @@ nvidia-smi
 
 echo "running training job:"
 
-# /usr/bin/python3 src/train.py \
-#                  experiment=$experiment_name \
-#                  paths.data_dir=$model_directory \
-#                  data.num_workers=$num_workers \
-#                  data.pin_memory=false \
-#                  data.batch_size=$batch_size \
-#                  trainer=ddp \
-#                  trainer.devices=$num_gpu_devices \
-#                  trainer.max_epochs=50 \
-#                  trainer.num_nodes=1
-
 export DS_SKIP_CUDA_CHECK=1
 export WANDB_API_KEY=b6cf381756cfeeb8e1d5a61ad946302465b56ad1
 
@@ -64,6 +53,34 @@ torchrun  --nproc_per_node=$num_gpu_devices --master_port=1212 train.py \
     --data_path datasets/drug_discovery/tabular/BROAD_REPURPOSING_DRUGS.json \
     --bf16 True \
     --output_dir $model_directory/darwin-7b_v2_finetuned \
+    --num_train_epochs 1 \
+    --per_device_train_batch_size 1 \
+    --per_device_eval_batch_size 1 \
+    --gradient_accumulation_steps 1 \
+    --evaluation_strategy "no" \
+    --save_strategy "steps" \
+    --save_steps 500 \
+    --save_total_limit 1 \
+    --learning_rate 2e-5 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --fsdp "full_shard auto_wrap" \
+    --fsdp_transformer_layer_cls_to_wrap 'LlamaDecoderLayer' \
+    --tf32 False
+
+# run locally
+# docker run --gpus '"device=6,7"' ivanjaenm/llm4bio
+# export CUDA_VISIBLE_DEVICES=6,7
+# export DS_SKIP_CUDA_CHECK=1
+# export WANDB_API_KEY=b6cf381756cfeeb8e1d5a61ad946302465b56ad1
+
+torchrun  --nproc_per_node=2 --master_port=1212 train.py \
+    --model_name_or_path models/darwin-7b_v2 \
+    --data_path datasets/drug_discovery/tabular/BROAD_REPURPOSING_DRUGS.json \
+    --bf16 True \
+    --output_dir models/darwin-7b_v2_finetuned \
     --num_train_epochs 1 \
     --per_device_train_batch_size 1 \
     --per_device_eval_batch_size 1 \
